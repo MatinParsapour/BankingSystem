0653d6f41eb6d3aac76da7a0794bf0010188f169
Matin <matin.parsapour.iam@gmail.com>
2021-09-18 18:30:42 +0430

    create cusomer main menu

--- src/main/java/util/DemonstrationMenus.java
+++ src/main/java/util/DemonstrationMenus.java
@@ -17,4 +17,17 @@ public class DemonstrationMenus {
         System.out.println("|       3.Back            |");
         System.out.println("+-------------------------+");
     }
+
+    public void customerMenu(){
+        System.out.println("+-------------------------+");
+        System.out.println("|    1.Open an account    |");
+        System.out.println("|     2.Card to card      |");
+        System.out.println("|  3.Transactions history |");
+        System.out.println("|         4.Balance       |");
+        System.out.println("|      5.Close account    |");
+        System.out.println("|         6.Log out       |");
+        System.out.println("|          7.Exit         |");
+        System.out.println("+-------------------------+");
+
+    }
 }
