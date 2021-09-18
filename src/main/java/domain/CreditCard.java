677b053e1dfca1277954c452eaae15c172af349b
Matin <matin.parsapour.iam@gmail.com>
2021-09-18 12:38:14 +0430

    create CreditCard entity

---
+++ src/main/java/domain/CreditCard.java
@@ -0,0 +1,57 @@
+package domain;
+
+import base.entity.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = CreditCard.TABLE_NAME)
+@Setter
+@Getter
+@NoArgsConstructor
+public class CreditCard extends BaseEntity<Long> {
+
+    public static final String TABLE_NAME = "credit_card_table";
+    private static final String BANK_NAME = "bank_name";
+    private static final String USER_SURE_NAME = "user_sure_name";
+    private static final String CARD_NUMBER = "card_number";
+    private static final String CVV2 = "cvv2";
+    private static final String EXPIRATION_DATE = "expiration_date";
+    private static final String BRANCH_CODE = "branch_code";
+    private static final String SHEBA_NUMBER = "sheba_number";
+    private static final String FIRST_PASSWORD = "first_password";
+    private static final String SECOND_PASSWORD = "second_password";
+
+    @JoinColumn(name = BANK_NAME)
+    private String bankName;
+
+    @JoinColumn(name = USER_SURE_NAME)
+    private String UserSureName;
+
+    @JoinColumn(name = CARD_NUMBER,unique = true)
+    private int cardNumber;
+
+    @JoinColumn(name = CVV2)
+    private int cVV2;
+
+    @JoinColumn(name = EXPIRATION_DATE)
+    private LocalDate expirationDate;
+
+    @JoinColumn(name = BRANCH_CODE)
+    private int branchCode;
+
+    @JoinColumn(name = SHEBA_NUMBER,unique = true)
+    private int shebaNumber;
+
+    @JoinColumn(name = FIRST_PASSWORD)
+    private int firstPassword;
+
+    @JoinColumn(name = SECOND_PASSWORD)
+    private int secondPassword;
+}
