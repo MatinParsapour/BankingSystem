5dd744bb60f8112b203531bb91df09167d2919fc
Matin <matin.parsapour.iam@gmail.com>
2021-09-18 13:08:26 +0430

    create BankBranch entity

---
+++ src/main/java/domain/BankBranch.java
@@ -0,0 +1,41 @@
+package domain;
+
+import base.entity.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = BankBranch.TABLE_NAME)
+@Setter
+@Getter
+@NoArgsConstructor
+public class BankBranch extends BaseEntity<Long> {
+    public static final String TABLE_NAME = "bank_branch";
+    private static final String CEO = "ceo";
+    private static final String EMPLOYEES = "employees";
+    private static final String BRANCH_CODE = "branch_code";
+    private static final String BANK_NAME = "bank_name";
+    private static final String ACCOUNTS = "accounts";
+
+    @OneToOne
+    @JoinColumn(name = CEO)
+    private CEO cEO;
+
+    @OneToMany
+    @JoinColumn(name = EMPLOYEES)
+    private Set<Employee> employees;
+
+    @JoinColumn(name = BRANCH_CODE)
+    private int branchCode;
+
+    @JoinColumn(name = BANK_NAME)
+    private String bankName;
+
+    @OneToMany
+    @JoinColumn(name = ACCOUNTS)
+    private Set<Account> accounts;
+}
