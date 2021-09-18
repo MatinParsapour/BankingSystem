f06f3ad80ecb565c03a66372baded794a5766ef5
Matin <matin.parsapour.iam@gmail.com>
2021-09-18 15:21:53 +0430

    create DemonstrationMenus

---
+++ src/main/java/util/DemonstrationMenus.java
@@ -0,0 +1,12 @@
+package util;
+
+public class DemonstrationMenus {
+    public void mainMenu(){
+        System.out.println("+--------------------------+");
+        System.out.println("|       1.CEO              |");
+        System.out.println("|       2.Employee         |");
+        System.out.println("|       3.Customer         |");
+        System.out.println("|       4.Exit             |");
+        System.out.println("+--------------------------+");
+    }
+}
